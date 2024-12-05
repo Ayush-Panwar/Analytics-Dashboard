7b5b0c008a8fbb07dbf944e6c514ce714cc4aeae
Ayush-Panwar <ayushpanwar717@gmail.com>
2024-12-05 17:09:36 +0530

    use credential in google auth when in production

--- lib/services/googleSheets.ts
+++ lib/services/googleSheets.ts
@@ -5,11 +5,27 @@ import path from "path";
 
 export class GoogleSheetsService {
   private static async getAuthClient() {
-    const keyFilePath = path.join(process.cwd(), "secrets.json");
-    return new google.auth.GoogleAuth({
-      keyFile: keyFilePath,
-      scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
-    });
+    if (process.env.NODE_ENV === "development") {
+      // Use the keyFile in development
+      const keyFilePath = path.join(process.cwd(), "secrets.json");
+      return new google.auth.GoogleAuth({
+        keyFile: keyFilePath,
+        scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
+      });
+    } else {
+      // Use credentials from environment variables in production
+      return new google.auth.GoogleAuth({
+        credentials: {
+          type: "service_account",
+          project_id: process.env.GOOGLE_PROJECT_ID,
+          private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+          private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+          client_email: process.env.GOOGLE_CLIENT_EMAIL,
+          client_id: process.env.GOOGLE_CLIENT_ID,
+        },
+        scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
+      });
+    }
   }
 
   public static async fetchData(): Promise<AnalyticsData[]> {
